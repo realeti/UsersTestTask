c843eddf7363050dff6e815d2985b4dbcbac57ff
realeti <oro.eto.bristol@gmail.com>
2025-09-26 09:49:07 +0300

    Initial Commit

---
+++ UsersTestTask/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  UsersTestTask
+//
+//  Created by realeti on 26.09.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ UsersTestTaskTests/UsersTestTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  UsersTestTaskTests.swift
+//  UsersTestTaskTests
+//
+//  Created by realeti on 26.09.2025.
+//
+
+import Testing
+@testable import UsersTestTask
+
+struct UsersTestTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
